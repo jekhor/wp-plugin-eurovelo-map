d31363cef5950738989efb1e5ea12139edaea993
uniorunr <gagael23@live.com>
2019-05-09 20:16:26 +0300

    fix: enable one finger scroll on mobile

--- scripts/map-root-style.js
+++ scripts/map-root-style.js
@@ -1,6 +1,7 @@
 const maps = Array.from(document.querySelectorAll('div[id^="eurovelo-wordpress-map-"]'));
 
 maps.forEach((map) => {
+  map.classList.add('leaflet-touch-zoom');
   map.style.width = 'calc(' + window.innerWidth + 'px - 12px)';
   map.style.maxWidth = window.innerWidth + 'px';
   map.style.marginLeft = 'calc(50% - 50vw + 8px)';
