8cd9132d2eab2810dfff91cb8eb2b162caa249ce
uniorunr <gagael23@live.com>
2019-06-16 12:47:13 +0300

    feature: open menu on label click in layers icon

--- scripts/after-map-load.js
+++ scripts/after-map-load.js
@@ -5,5 +5,12 @@ const label = document.createElement('span');
 label.className = 'control-layers-label';
 label.textContent = 'Слои';
 
+const labelClick = ({ currentTarget }) => {
+  currentTarget.parentElement.classList.add('leaflet-control-layers-expanded');
+};
+
+label.addEventListener('click', labelClick);
+label.addEventListener('touchstart', labelClick);
+
 layerIcon.appendChild(label);
 
